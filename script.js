e0e1d8f9754057f33a78d5abc2d0e07fae2e07c1
unknown <madhusoodan@panipurisoft.com>
2017-10-10 15:13:40 +0530

    jkgh

---
+++ script.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+    angular.module("myApp", [])
+        .controller('myController', function($scope) {
+            var ctrl = this;
+            ctrl.name = "madhu";
+            console.log($scope);
+        });
+})();
